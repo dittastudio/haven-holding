a922dc7c084b5784c77960f50779d4bba2defa2c
Will The Dev <hello@willthe.dev>
2025-09-18 12:24:44 +0100

    add resolvers

--- app/components/storyblok/Text.vue
+++ app/components/storyblok/Text.vue
@@ -1,5 +1,16 @@
 <script lang="ts" setup>
 import type { StoryblokRichtext } from '@@/.storyblok/types/storyblok'
+import type { StoryblokRichTextNode } from '@storyblok/vue'
+import { NuxtLink } from '#components'
+
+const resolvers = {
+  [MarkTypes.LINK]: (node: StoryblokRichTextNode<VNode>) =>
+    h(NuxtLink, {
+      to: storyblokSlug(node.attrs?.href),
+      target: node.attrs?.target,
+    }, { default: () => node.text ?? '' }),
+  [BlockTypes.BR]: () => h('br'),
+}
 
 interface Props {
   content?: StoryblokRichtext | undefined
@@ -12,5 +23,6 @@ const { content } = defineProps<Props>()
   <StoryblokRichText
     v-if="content"
     :doc="content"
+    :resolvers="resolvers"
   />
 </template>
