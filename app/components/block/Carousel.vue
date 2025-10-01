21b71c6c4824e1e6727cd4904faed954ac5a5867
Michael Pumo <hello@michaelpumo.com>
2025-10-01 14:08:01 +0100

    Improve all the shizzle.

--- app/components/block/Carousel.vue
+++ app/components/block/Carousel.vue
@@ -60,21 +60,21 @@ const slides = [
   },
 ]
 
-const primary = useTemplateRef('primary')
+const main = useTemplateRef('main')
+const container = useTemplateRef('container')
 const image = useTemplateRef('image')
 const text = useTemplateRef('text')
-const secondary = useTemplateRef('secondary')
 
 const tl = ref<gsap.core.Timeline | null>(null)
 
 const retrigger = ref(0)
 
-const carouselRef = useTemplateRef<Carousel>('carouselRef')
-const carouselDetails = computed(() => carouselRef.value?.carousel.details.value)
-const carouselCurrentSlide = computed(() => carouselRef.value?.carousel.slider.value?.slides[carouselDetails.value?.abs || 0])
+const carousel = useTemplateRef<Carousel>('carousel')
+const carouselDetails = computed(() => carousel.value?.carousel.details.value)
+const carouselCurrentSlide = computed(() => carousel.value?.carousel.slider.value?.slides[carouselDetails.value?.abs || 0])
 const carouselCurrentMedia = computed(() => carouselCurrentSlide.value?.querySelector('img'))
 const carouselCurrentProperties = computed(() => {
-  const slide = carouselRef.value?.carousel.slider.value.slides[carouselDetails.value?.abs || 0]
+  const slide = carousel.value?.carousel.slider.value.slides[carouselDetails.value?.abs || 0]
   const media = slide?.querySelector('img')
 
   if (!media) {
@@ -90,7 +90,7 @@ const carouselCurrentProperties = computed(() => {
     height,
     top,
     left,
-    relativeTop: top - (secondary.value ? secondary.value?.getBoundingClientRect().top : 0) || 0,
+    relativeTop: top - (container.value ? container.value?.getBoundingClientRect().top : 0) || 0,
   }
 })
 
@@ -103,9 +103,9 @@ const sequenceText = () => {
 
   gsap.timeline({
     scrollTrigger: {
-      trigger: primary.value,
+      trigger: main.value,
       start: 'top top',
-      end: 'bottom top',
+      end: '50% top',
       scrub: true,
       markers: false,
     },
@@ -130,9 +130,9 @@ const sequenceMedia = () => {
 
   tl.value = gsap.timeline({
     scrollTrigger: {
-      trigger: secondary.value,
-      start: 'top bottom',
-      end: 'top top',
+      trigger: main.value,
+      start: '50% top',
+      end: '75% top',
       scrub: true,
       markers: false,
       invalidateOnRefresh: true,
@@ -192,7 +192,6 @@ watch(
     carouselCurrentProperties.value,
   ],
   () => {
-    console.log('carouselCurrentProperties changed')
     return requestRefresh.restart(true)
   },
   {
@@ -204,18 +203,22 @@ watch(
 
 <template>
   <div
+    ref="main"
     v-editable="block"
-    class="block-carousel relative bg-white"
+    class="block-carousel relative bg-white h-[500vh]"
   >
-    <pre class="fixed top-10 right-10 z-50 bg-black/50 text-white text-12 p-4 max-h-[50vh] overflow-scroll rounded-xl backdrop-blur-2xl">
+    <!-- <pre class="fixed top-10 right-10 z-50 bg-black/50 text-white text-12 p-4 max-h-[50vh] overflow-scroll rounded-xl backdrop-blur-2xl">
 {{ carouselCurrentProperties }}
-</pre>
+</pre> -->
 
-    <div class="pointer-events-none absolute inset-0 z-1 size-full">
-      <div class="sticky top-0 w-full h-screen">
+    <div
+      ref="container"
+      class="sticky inset-0 z-1 w-full h-screen"
+    >
+      <div class="absolute inset-0 z-20 size-full pointer-events-none">
         <div
           ref="image"
-          class="absolute inset-0 size-full"
+          class="absolute inset-0 z-20 size-full"
         >
           <img
             v-if="carouselCurrentMedia"
@@ -225,13 +228,8 @@ watch(
           >
         </div>
       </div>
-    </div>
 
-    <div class="relative z-10 w-full h-[200vh]">
-      <div
-        ref="primary"
-        class="sticky top-0 w-full h-screen"
-      >
+      <div class="absolute inset-0 z-30 size-full pointer-events-none">
         <p
           ref="text"
           class="size-full type-mono-30-70 px-(--app-outer-gutter) py-[calc(var(--app-outer-gutter)*1.5)] md:p-[5%] flex flex-col justify-between text-white bg-black/50"
@@ -245,48 +243,45 @@ watch(
           <span class="self-end">creation</span>
         </p>
       </div>
-    </div>
 
-    <div
-      ref="secondary"
-      class="flex flex-col justify-center h-screen overflow-hidden pt-[calc(var(--app-vertical-rhythm)_*_2)] pb-(--app-vertical-rhythm)"
-    >
-      <h2 class="type-mono-12 md:type-mono-14 text-center mb-[calc(var(--app-vertical-rhythm)_*_0.5)]">
-        The Space
-      </h2>
+      <div class="absolute inset-0 z-10 size-full">
+        <div class="flex flex-col justify-center h-screen overflow-hidden pt-[calc(var(--app-vertical-rhythm)_*_2)] pb-(--app-vertical-rhythm)">
+          <h2 class="type-mono-12 md:type-mono-14 text-center mb-[calc(var(--app-vertical-rhythm)_*_0.5)]">
+            The Space
+          </h2>
 
-      <UiCarousel
-        ref="carouselRef"
-        :items="slides"
-        class="shrink-0"
-      >
-        <template #item="{ item }">
-          <div
-            class="block-carousel__item size-full px-(--app-outer-gutter) flex items-center justify-center"
+          <UiCarousel
+            ref="carousel"
+            :items="slides"
+            class="shrink-0"
           >
-            <img
-              :src="item.image"
-              :alt="item.caption"
-              class="block size-auto max-w-full max-h-full"
+            <template #item="{ item }">
+              <div class="block-carousel__item size-full px-(--app-outer-gutter) flex items-center justify-center">
+                <img
+                  :src="item.image"
+                  :alt="item.caption"
+                  class="block size-auto max-w-full max-h-full"
+                >
+              </div>
+            </template>
+          </UiCarousel>
+
+          <div
+            v-if="typeof carouselDetails?.abs === 'number'"
+            class="wrapper flex gap-x-(--app-inner-gutter) my-[calc(var(--app-vertical-rhythm)_*_0.25)] type-mono-12 md:type-mono-14"
+          >
+            <p class="w-1/2 text-right">
+              {{ carouselDetails.abs + 1 }}/{{ carouselDetails.length + 1 }}
+            </p>
+
+            <p
+              v-if="slides[carouselDetails.abs]?.caption"
+              class="w-1/2"
             >
+              {{ slides[carouselDetails.abs]?.caption }}
+            </p>
           </div>
-        </template>
-      </UiCarousel>
-
-      <div
-        v-if="typeof carouselDetails?.abs === 'number'"
-        class="wrapper flex gap-x-(--app-inner-gutter) my-[calc(var(--app-vertical-rhythm)_*_0.25)] type-mono-12 md:type-mono-14"
-      >
-        <p class="w-1/2 text-right">
-          {{ carouselDetails.abs + 1 }}/{{ carouselDetails.length + 1 }}
-        </p>
-
-        <p
-          v-if="slides[carouselDetails.abs]?.caption"
-          class="w-1/2"
-        >
-          {{ slides[carouselDetails.abs]?.caption }}
-        </p>
+        </div>
       </div>
     </div>
   </div>
