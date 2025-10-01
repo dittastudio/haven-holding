5f4c079e98d54a28dda7fa647eed27837a3cc997
Will The Dev <hello@willthe.dev>
2025-10-01 11:51:19 +0100

    Revert "bollocks"
    This reverts commit 38c7113676bdf56bc8e75cadd92cb17566caab5d.

--- app/components/app/Blocks.vue
+++ app/components/app/Blocks.vue
@@ -66,7 +66,7 @@ const checkBackgroundMatchesPrevBackground = (index: number) => {
       :block="block"
     />
 
-    <BlockCarouselV2
+    <BlockCarousel
       v-else-if="block.component === 'block_carousel'"
       :block="block"
     />
